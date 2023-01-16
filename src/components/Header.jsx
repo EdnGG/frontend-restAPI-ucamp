10bc227672865600cd7bc0ca321abb13ec4f587f
Eden <gresseden@gmail.com>
2023-01-15 21:23:44 -0500

    working on styling components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,30 +2,38 @@ import { Link, Outlet } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="">
-      <nav className="navbar navbar-expand-lg bg-light">
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-light ">
+        {/* image, title and subtitle */}
+        <div className="d-flex px-3 py-3 justify-content-start justify-content-sm-around align-items-center">
+          <img src="https://picsum.photos/100/100?grayscale" className="rounded" alt="Brand" />
+          <Link className="px-2 navbar-brand" to="/">GUITARWORLD</Link>
+          <Link className="px-2 navbar-brand nav-link" to="/guitars">Catalog</Link>
+        </div>
+        
+
         <div className="container">
-          <img src="https://picsum.photos/50/50?grayscale" className="rounded" alt="Brand" />
-          <Link className="px-2 navbar-brand w-45 justify-content-start" to="/">GUITARWORLD</Link>
-          <Link className="nav-link active p-3" aria-current="page" to="/guitars">Catalog</Link>
+
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 
-          <div className="navbar-nav m-2 justify-content-start w-100">
-              {/* <Link className="nav-link active p-3" aria-current="page" to="/guitars">Catalog</Link> */}
-              
-           
-              
-            </div>
-            <div className="d-flex justify-content-end w-100">
-            <Link className="nav-link p-3" to="/DFSFDS">USD</Link>
-            <Link className="nav-link p-3" to="/login">Login</Link>
-            <Link className="nav-link p-3 " to="/signin">Sign In</Link>
-            </div>
-            
-        </div>
+              <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+
+              {/* <div className="navbar-nav m-2 justify-content-end w-100"> */}
+                <Link className="nav-link p-3" to="/ddssdf">USD</Link>
+                <Link className="nav-link p-3" to="/login">Login</Link>
+                <Link className="nav-link p-3 " to="/signin">Sign In</Link>
+              {/* </div> */}
+                
+            {/* <div className="d-flex justify-content-end w-100">
+                <Link className="nav-link p-3" to="/DFSFDS">USD</Link>
+                <Link className="nav-link p-3" to="/login">Login</Link>
+                <Link className="nav-link p-3 " to="/signin">Sign In</Link>
+              </div> */}
+                
+              </div>
+          
       </div>
     </nav>
   </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,18 @@ import '../styles/Home.css'
 
 const Home = () => {
   return (
-    <div className="container__image">
+    <div className="container-fluid">
+      <div className="container__image">
+
+        <h1 className="display-4 fw-bolder">Guitar of your dreams is waiting for you!!</h1>
+        {/* <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p> */}
+        <hr className="my-4"></hr>
+        <p className="fw-bold">We got the most exted catalog of all continent. We ship everywere in the world.</p>
+        <p className="lead">
+          <a className="btn btn-primary btn-lg" href="#" role="button">See Catalog</a>
+        </p>
+      </div>
       
-      <h1 className="display-4">Hello, world!</h1>
-      <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-      <hr className="my-4"></hr>
-      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-      <p className="lead">
-        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </p>
     </div>
   )
 }
