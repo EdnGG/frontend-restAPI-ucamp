5e1d55bf5ced22273c9226845bcd421990482034
Eden <gresseden@gmail.com>
2023-02-01 23:23:08 -0500

    working on footer component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap"; // Linea importante para componentres de bootstrap
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
-import { UserProvider } from './context/UserContext';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import { UserProvider } from "./context/UserContext";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
-    <BrowserRouter>
-      <App />
+      <BrowserRouter>
+        <App />
       </BrowserRouter>
     </UserProvider>
   </React.StrictMode>
